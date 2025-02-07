c71939a47d06f8810c01ffe03a5dbe8b3015091c
Onur Sezen <onur@palnea.com>
2025-02-08 01:07:17 +0300

    Skip middleware for RSC

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,28 @@ import nextI18NextConfig from './next-i18next.config.js';
 
 const nextConfig = {
   ...nextI18NextConfig,
-  output: 'standalone',
-  experimental: {
-    appDir: true,
+  reactStrictMode: false,
+  basePath: process.env.BASEPATH,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+        locale: false
+      },
+    ]
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_APP_URL, // Replace this with the hostname of your image source
+        port: '',
+        pathname: '/**' // This will allow all image paths from this domain
+      }
+    ]
+  }
 }
 
-export default nextConfig;
-
-// export default {
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/:path*',
-//         destination: 'http://localhost:7153/:path*'
-//       }
-//     ]
-//   }
-// }
+export default nextConfig
