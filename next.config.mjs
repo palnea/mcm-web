900f3adcf0dd83f9d7cab2d31f22e113c3170f92
Onur Sezen <onur@palnea.com>
2025-02-08 01:17:32 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,26 +5,6 @@ const nextConfig = {
   ...nextI18NextConfig,
   reactStrictMode: false,
   basePath: process.env.BASEPATH,
-  redirects: async () => {
-    return [
-      {
-        source: '/',
-        destination: '/login',
-        permanent: true,
-        locale: false
-      },
-    ]
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_APP_URL, // Replace this with the hostname of your image source
-        port: '',
-        pathname: '/**' // This will allow all image paths from this domain
-      }
-    ]
-  }
 }
 
 export default nextConfig
