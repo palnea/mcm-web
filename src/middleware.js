7746292da4436b7478fdff416d9fc11282e44132
Onur Sezen <onur@palnea.com>
2025-02-08 00:54:12 +0300

    update matcher

--- src/middleware.js
+++ src/middleware.js
@@ -36,7 +36,8 @@ export function middleware(request) {
 
 export const config = {
   matcher: [
-    '/((?!api/|_next/static|_next/image|favicon.ico|images/|.*\\.(png|jpg|jpeg|gif|webp|svg|ico)$).*)',
+    '/((?!api/|_next/static|_next/image|favicon.ico|images).*)',
   ],
 };
+
 
