1f8c426766b348c5a25fd46ef5cdbdd056b3ef45
Onur Sezen <onur@palnea.com>
2025-02-08 01:13:56 +0300

    Skip middleware for RSC

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,17 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  console.log('in middleware>>>')
-  console.log('Request URL:', request.url)
-  console.log('Request Pathname:', request.nextUrl.pathname)
+  console.log('in middleware>>>');
+  console.log('Request URL:', request.url);
+  console.log('Request Pathname:', request.nextUrl.pathname);
 
-  return NextResponse.next()
+  // Skip middleware for RSC (React Server Components) requests
+  if (request.nextUrl.searchParams.has('_rsc')) {
+    console.log('Skipping middleware for _rsc request');
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
