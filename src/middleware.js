8ccd2ec3bb28c9f0b3044ff48e6a64df28e87255
Onur Sezen <onur@palnea.com>
2025-02-08 01:03:59 +0300

    set bare minimum middleware config

--- src/middleware.js
+++ src/middleware.js
@@ -1,35 +1,9 @@
 import { NextResponse } from 'next/server'
 
-const unprotectedRoutes = ['/login']
-
 export function middleware(request) {
   console.log('in middleware>>>')
-  const { nextUrl } = request
-  const pathname = nextUrl.pathname
-
-  if (pathname.startsWith('/images/') || /\.(png|jpg|jpeg|gif|webp|svg|ico)$/i.test(pathname)) {
-    return NextResponse.next()
-  }
-
-  const token = request.cookies.get('authToken')
-  console.log('token: ', token)
-  const isAuthenticated = token && token.value && token.value.length > 0
-  console.log('isAuthenticated: ', isAuthenticated)
-  console.log('request url: ', request.url)
-
-  if (nextUrl.pathname === '/') {
-    return isAuthenticated
-      ? NextResponse.redirect(new URL('/dashboard', request.url))
-      : NextResponse.redirect(new URL('/login', request.url))
-  }
-
-  if (nextUrl.pathname === '/login' && isAuthenticated) {
-    return NextResponse.redirect(new URL('/dashboard', request.url))
-  }
-
-  if (!unprotectedRoutes.includes(nextUrl.pathname) && !isAuthenticated) {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
+  console.log('Request URL:', request.url)
+  console.log('Request Pathname:', request.nextUrl.pathname)
 
   return NextResponse.next()
 }
@@ -39,5 +13,3 @@ export const config = {
     '/((?!api/|_next/static|_next/image|favicon.ico|images).*)',
   ],
 };
-
-
